0bd5277fce0286da2f95288c72ac078b6adfac87
boroknagyz <boroknagyz@cloudera.com>
2024-03-28 20:33:03 +0100

    Core: default parquet compression codec to snappy (#290)
    Since Iceberg 1.4 we also need to set
    PARQUET_COMPRESSION_DEFAULT_SINCE_1_4_0 to 'snappy', not just
    PARQUET_COMPRESSION_DEFAULT.

--- core/src/main/java/org/apache/iceberg/TableProperties.java
+++ core/src/main/java/org/apache/iceberg/TableProperties.java
@@ -146,7 +146,7 @@ public class TableProperties {
   public static final String PARQUET_COMPRESSION = "write.parquet.compression-codec";
   public static final String DELETE_PARQUET_COMPRESSION = "write.delete.parquet.compression-codec";
   public static final String PARQUET_COMPRESSION_DEFAULT = "snappy";
-  public static final String PARQUET_COMPRESSION_DEFAULT_SINCE_1_4_0 = "zstd";
+  public static final String PARQUET_COMPRESSION_DEFAULT_SINCE_1_4_0 = "snappy";
 
   public static final String PARQUET_COMPRESSION_LEVEL = "write.parquet.compression-level";
   public static final String DELETE_PARQUET_COMPRESSION_LEVEL =
